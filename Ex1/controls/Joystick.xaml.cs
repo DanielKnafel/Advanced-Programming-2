3415e59b15ccb2e00d6f88e401fa92639977b9d9
DanielKnafel <52681699+DanielKnafel@users.noreply.github.com>
2021-03-27 14:52:12 +0300

    Joystick almost working

---
+++ Ex1/controls/Joystick.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace Ex1.controls
+{
+    public partial class Joystick : UserControl
+    {
+        private JoystickViewModel vm;
+        double size, angle;
+        public Joystick()
+        {
+            InitializeComponent();
+            JoystickInner.Margin = new Thickness(109, 56, 0, 0);
+            double radius = JoystickMiddle.Margin.Left + JoystickInner.Width / 2 - JoystickMiddle.Margin.Left;
+            this.vm = new JoystickViewModel(new JoystickModel(JoystickInner.Margin, radius));
+            //vm.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e){ JoystickInner.Margin = vm.VM_NewLocation; };
+            DataContext = vm;
+        }
+
+        public double Joystick_Size
+        {
+            set
+            {
+                this.size = value;
+                vm.Size = value;
+            }
+        }
+        public double Joystick_Angle
+        {
+            set
+            {
+                this.angle = value;
+                vm.Angle = value;
+            }
+        }
+    }
+}
---
+++ Ex1/controls/JoystickModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Ex1.controls
+{
+    public class JoystickModel : INotifyPropertyChanged
+    {
+        private double radios;
+        private Thickness defaultLocation;
+        private Thickness newLocation;
+        
+        public JoystickModel(Thickness defaultLocation, double radius)
+        {
+            this.defaultLocation = defaultLocation;
+            this.newLocation = defaultLocation;
+            this.radios = radius;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public Thickness NewLocation
+        {
+            get { return this.newLocation; }
+            set
+            {
+                this.newLocation = value;
+                NotifyPropertyChanged("NewLocation");
+            }
+        }
+        public Thickness DefaultLocation
+        {
+            set
+            {
+                this.defaultLocation = value;
+            }
+        }
+        
+        // moves the inner joystick to the given position. 0 <= size <= 1, 0 <= angle < 360
+        public void moveJoystick(double size, double angle)
+        {
+            angle *= Math.PI / 180;
+            size *= this.radios;
+            double newCenterX = size * Math.Sin(angle);
+            double newCenterY = size * Math.Cos(angle);
+            this.newLocation = new Thickness(this.defaultLocation.Left + newCenterX, this.defaultLocation.Top - newCenterY, this.defaultLocation.Right, this.defaultLocation.Bottom);
+            NotifyPropertyChanged("NewLocation");
+        }
+    }
+}
+
